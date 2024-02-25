ce3a9b64abbbe6cbc5f0fca83ab171ba620cff81
Emily Liu <emilyliu@Emilys-MacBook-Air.local>
2024-02-25 20:54:26 +1100

    formatted jsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,142 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+import React, { useState, useEffect, useRef } from 'react';
+import styles from './styles.module.scss'
+import { FaRegHeart, FaHeart, FaCat } from "react-icons/fa";
+import { MdDeleteOutline } from "react-icons/md";
+
+type CatFact = {
+  fact: string, 
+  length?: number,
+  isFavourited?: boolean
+};
+interface CatFactsInfo {
+  number: number, 
+  fact: string, 
+  onRemove: () => void;
+  onFav: () => void;
+  isFav: boolean | undefined; 
+}
+
+const API_URL: string  = 'https://catfact.ninja/facts'
+
+const CatFactsPage: React.FC<any> = (props) => {
+  const [catFacts, setCatFacts] = useState([] as CatFact[]);
+  const [modalState, setModalState] = useState(false);
+  const [newFact, setNewFact] = useState('');
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((object) => {
+        const savedFacts = JSON.parse(localStorage.getItem("favFacts") || "[]");
+        object.data.map((catFact: CatFact) => {
+          catFact.fact = catFact.fact;
+          catFact.isFavourited = savedFacts.includes(catFact.fact);
+        })
+        setCatFacts(object.data.slice(0, 5));
+      })
+  }, []);
+
+  function onRemove(idx: number) {
+    const savedFacts = JSON.parse(localStorage.getItem("favFacts") || "[]");
+    localStorage.setItem("favFacts", JSON.stringify(savedFacts.filter((fact: string) => fact != catFacts[idx].fact)));
+    const updatedFactsArray = catFacts.slice(0, idx).concat(catFacts.slice(idx + 1));
+    setCatFacts(updatedFactsArray);
+  }
+
+  function onFav(idx: number) {
+    const allCatFacts = [...catFacts]
+    const catObj = allCatFacts[idx];
+    catObj.isFavourited = !catObj.isFavourited;
+    setCatFacts(allCatFacts);
+    const updatedFactArray = catFacts.filter((catFact) => catFact.isFavourited).map((catFact) => catFact.fact);
+    localStorage.setItem("favFacts", JSON.stringify(updatedFactArray));
+  }
+
+  const openModal = () => setModalState(true);
+  const closeModal = () => setModalState(false);
+
+  function addFact() {
+    setCatFacts([...catFacts, {fact: newFact, isFavourited: false}]);
+    closeModal();
+  }
+
+  const renderDialog = () => { 
+    return (
+        <dialog className={styles.modal}>
+          <form onSubmit={addFact} className={styles.form}>
+            <h1 className={styles.modalHeader}>Add your own cat fact!</h1>
+            <input 
+              type="text"
+              placeholder='Enter fact here..'
+              onChange={(e) => setNewFact(e.target.value)} 
             />
-          </a>
+              <div
+                className={styles.modalButtonsContainer}>
+                <button type='submit'>Add fact</button>
+                <button
+                onClick={closeModal}>
+                Cancel
+                </button>
+              </div>
+          </form>
+        </dialog>
+    )
+  }
+
+  return (
+    <div className={styles.main}>
+      <h1 className={styles.title}>Some cool cat facts:</h1>
+      <div className={styles.factsContainer}>
+        {catFacts.map((catFact: CatFact, idx: number) => {
+          return (
+            <CatFacts
+              key={idx}
+              number={idx}
+              fact={catFact.fact}
+              onRemove={() => onRemove(idx)}
+              onFav={() => onFav(idx)}
+              isFav={catFact.isFavourited} 
+            />
+          )})}
+      </div>
+      {modalState && renderDialog()}
+      <div className={styles.addButtonContainer}>
+        <button
+          className={styles.fab}
+          onClick={openModal}>
+            Add fact!
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const CatFacts = (props: CatFactsInfo) => { 
+  const { number, fact, onRemove, onFav, isFav } = props;
+
+  return (
+    <div className={styles.factCard}>
+    <div className={styles.factContent}>
+      <FaCat className={styles.catIcon} size={100} />
+      <div className={styles.factText}>
+        <h1 className={styles.factNumber}>{`Fact #${number + 1}`}</h1>
+        <h1>{fact}</h1>
+        <div className={styles.factButtonContainer}>
+          <button onClick={onRemove}>
+            <MdDeleteOutline size={30} />
+          </button>
+          <button
+            className={isFav ? styles.fav : styles.unfav}
+            onClick={onFav}>
+            {isFav ? (<FaHeart size={25} className={styles.fav} />) : (<FaRegHeart size={25} />)}
+          </button>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
+  </div>
   )
-}
+};
+
+export default CatFactsPage;
